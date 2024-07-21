22b1bb39b8e68183e0a9234e21f6070f87ac1e36
vladlanov <bulanov1192@gmail.com>
2024-07-21 13:58:58 +0500

    Add .gitignore and remove node_modules and dist from tracking

--- dist/templates/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return (
-    <main>
-      <h1>This is your new app's base. Make something cool with it xD</h1>
-    </main>
-  );
-}
